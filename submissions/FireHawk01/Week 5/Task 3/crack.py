82a25056cd988862293ebf790981a04af9afc657
Vivek Kumar <44600209+FireHawk01@users.noreply.github.com>
2019-06-29 00:23:06 +0530

    Create crack.py

---
+++ submissions/FireHawk01/Week 5/Task 3/crack.py
@@ -0,0 +1,19 @@
+def main():
+
+	zipfilename = 'test.zip'
+	dictionary = 'p.txt'
+
+	password = None
+	zip_file = zipfile.ZipFile(zipfilename)
+	with open(dictionary, 'r') as f:
+		for line in f.readlines():
+			password = line.strip('\n')
+			try:
+				zip_file.extractall(pwd=password)
+				password = 'Password found: %s' % password
+			except:
+				pass
+	print password
+
+if __name__ == '__main__':
+	main()
